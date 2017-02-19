3b568811d5dbf85a34735625176011f98384505c
Dustin Johnson <dustin.r.johnson@gmail.com>
2017-02-18 22:14:36 -0500

    Validated build and made updates to build for Kernel 3.14 using UDOO Kernel from Git.

--- board/udoo/neo_common/post-build.sh
+++ board/udoo/neo_common/post-build.sh
@@ -2,7 +2,12 @@
 
 BOARD_DIR="$(dirname $0)"
 
-$HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none  \
--n "boot script" -d $BOARD_DIR/boot.scr.txt $BOARD_DIR/boot.scr
+echo $HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none  \
+-n "boot script" -d $BOARD_DIR/boot.scr.txt $BINARIES_DIR/boot.scr
 
-install -m 0644 -D $BOARD_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+echo "Creating Image boot.scr"
+$HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none  \
+-n "boot script" -d $BOARD_DIR/boot.scr.txt $BINARIES_DIR/boot.scr
+
+echo "Installing to $TARGET_DIR"
+install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
